9312819f0b573fd3abcb87aa237ab7ddf3179e5d
Lucca <luccamedina03@gmail.com>
2026-03-17 18:36:35 -0300

    feat: setting neon database

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from '@/app/generated/prisma/client'
 import { PrismaPg } from '@prisma/adapter-pg'
+import { Pool } from 'pg' // <-- Agrega esta importación
 
 // Singleton pattern — prevents multiple instances during Next.js hot-reload in dev
 const globalForPrisma = globalThis as unknown as {
@@ -7,10 +8,16 @@ const globalForPrisma = globalThis as unknown as {
 }
 
 function createPrismaClient() {
-  const adapter = new PrismaPg({
-    connectionString: process.env.DATABASE_URL!,
+  // 1. Instanciamos el Pool de conexiones de 'pg'
+  const pool = new Pool({ 
+    connectionString: process.env.DATABASE_URL! 
   })
 
+  // 2. Le pasamos el pool al adaptador de Prisma
+  // @ts-expect-error: Prisma espera tipos de pg 8.11 pero usamos 8.18
+    const adapter = new PrismaPg(pool)
+    
+  // 3. Inicializamos el cliente
   return new PrismaClient({
     adapter,
     log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
